dcb41883b6cc8024120cfca5ba7ac11e2f20b61d
Echoting <870917335@qq.com>
2020-10-26 17:04:43 +0800

    chore: 增加loginCheck中间件

--- blogExpress/routes/blog.js
+++ blogExpress/routes/blog.js
@@ -13,7 +13,21 @@ const {SuccessModel, ErrorModel} = require('../model/resModel.js')
 
 /* GET home page. */
 router.get('/list', function(req, res, next) {
-	const {keyword, author} = req.query
+	let {keyword, author} = req.query
+
+	// 管理员页面
+	if (req.query.isadmin) {
+        if (!req.session.username) {
+            res.json(
+                new ErrorModel('尚未登录')
+            )
+        }
+
+        // 管理员强制查询自己的博客
+        author = req.session.username
+	}
+
+
 	const result = getBlogList(author, keyword)
 
 	return result.then(listData => {
